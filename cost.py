fe478a2a2ef27074c48e20ba9c57764a9f852433
Colin Hobday <colin@hobday.online>
2023-04-24 13:34:20 +0800

    extracted out variables that are likely to change

--- cost.py
+++ cost.py
@@ -1,44 +1,37 @@
 import boto3
 from datetime import datetime, timedelta
 
-client = boto3.client('ce', 'us-east-1')
+def get_daily_cost_detail(start_date, end_date, dimensions) :
+    client = boto3.client('ce', 'us-east-1')
+
+    # api documentation - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ce/client/get_cost_and_usage.html
+    response = client.get_cost_and_usage(
+        TimePeriod={
+            'Start': start_date.strftime('%Y-%m-%d'),
+            'End': end_date.strftime('%Y-%m-%d')
+        },
+        Granularity='DAILY',
+        Metrics=['AmortizedCost'], # Explanation -  https://aws.amazon.com/blogs/aws-cloud-financial-management/understanding-your-aws-cost-datasets-a-cheat-sheet/
+        GroupBy = [ {
+            'Type':"DIMENSION",
+            'Key': dimension,
+        } for dimension in dimensions ],
+    )
+
+    if 'NextPageToken' in response : 
+        raise ValueError('Need to handle for multiple pages!') # TODO - multiple pages
+
+    return response
+
 
 yesterday = datetime.today() - timedelta(days=1)
 start_date = yesterday.replace(hour=0,minute=0, second=0,microsecond=0)
 end_date = start_date + timedelta(days=1)
 
-# Valid dimensions - https://docs.aws.amazon.com/aws-cost-management/latest/APIReference/API_GetDimensionValues.html
+dimensions = ["SERVICE", "USAGE_TYPE"]
 
-response = client.get_cost_and_usage(
-    TimePeriod={
-        'Start': start_date.strftime('%Y-%m-%d'),
-        'End': end_date.strftime('%Y-%m-%d')
-    },
-    Granularity='DAILY',
-    Metrics=['UnblendedCost'],
-    GroupBy=[
-        {
-        'Type':"DIMENSION",
-        'Key':"SERVICE",
-        },
-        {
-        'Type':"DIMENSION",
-        'Key':"USAGE_TYPE",
-        },
-    ],
-    # TODO multiple pages - lower priority for now
-)
+response = get_daily_cost_detail(start_date, end_date, dimensions)
 
 import json
 print( json.dumps(response, indent=2))
 
-import sys
-sys.exit(0)
-
-for result in response['ResultsByTime']:
-    print(f"Date: {result['TimePeriod']['Start']} - {result['TimePeriod']['End']}")
-    print(f"Total Cost: {result['Total']['UnblendedCost']['Amount']}")
-    print(f"Cost by Service: ")
-    for cost in result['Groups']:
-        print(f"{cost['Keys'][0]}: {cost['Metrics']['UnblendedCost']['Amount']}")
-    print()
