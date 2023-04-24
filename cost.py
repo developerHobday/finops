4a58534256460b9a745d8cfbee619a0b9f33ca1e
Colin Hobday <colin@hobday.online>
2023-04-24 13:54:36 +0800

    write to s3 change parameters to 100 days data

--- cost.py
+++ cost.py
@@ -1,7 +1,9 @@
 import boto3
 from datetime import datetime, timedelta
 
+
 def get_daily_cost_detail(start_date, end_date, dimensions) :
+    print(f'Start get_daily_cost_detail for {start_date} {end_date} {dimensions}')
     client = boto3.client('ce', 'us-east-1')
 
     # api documentation - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ce/client/get_cost_and_usage.html
@@ -18,20 +20,46 @@ def get_daily_cost_detail(start_date, end_date, dimensions) :
         } for dimension in dimensions ],
     )
 
+
+
     if 'NextPageToken' in response : 
         raise ValueError('Need to handle for multiple pages!') # TODO - multiple pages
 
     return response
 
 
+def get_day(date) :
+    return date.replace(hour=0,minute=0, second=0,microsecond=0)
+
 yesterday = datetime.today() - timedelta(days=1)
 start_date = yesterday.replace(hour=0,minute=0, second=0,microsecond=0)
 end_date = start_date + timedelta(days=1)
+
+start_date = get_day( datetime.today() - timedelta(days=100) )
+end_date = get_day(datetime.today())
+
+
 
 dimensions = ["SERVICE", "USAGE_TYPE"]
 
 response = get_daily_cost_detail(start_date, end_date, dimensions)
 
-import json
-print( json.dumps(response, indent=2))
+def write_to_s3(filename, body) :
+    print('Start write_to_s3')
+    import json
+    # print( json.dumps(response, indent=2))
+    s3 = boto3.resource('s3')
+    bucket_name = 'finops-738662219515'
+    bucket = s3.Bucket(bucket_name) 
+
+    obj = bucket.Object(f'cost/{filename}.json')
+    obj.put(
+        Body=(bytes(json.dumps(body).encode('UTF-8')))
+    )
+
+filename = f'{start_date.strftime("%Y%m%d")}-{end_date.strftime("%Y%m%d")}'
+write_to_s3(filename, response)
+
+
+
 
